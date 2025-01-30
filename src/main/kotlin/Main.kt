ac31434dbd062a37a1ad3d9431d52d7cdd2784ef
max <nickitkacozloff@yandex.ru>
2025-01-31 00:57:41 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,29 @@
+package org.example
+
+fun main() {
+    val playerField = Array(10) { Array(10) { 0 } }
+    val computerField = Array(10) { Array(10) { 0 } }
+
+    println("Добро пожаловать в игру \"Морской бой\"!")
+    printField(playerField)
+}
+
+fun printField(field: Array<Array<Int>>) {
+    val header = "  А  Б  В  Г  Д  Е  Ж  З  И  К"
+    println(header)
+
+    for (i in field.indices) {
+        print("${i + 1} ".padEnd(2))
+        for (cell in field[i]) {
+            val symbol = when (cell) {
+                0 -> "." // пустая ячейка
+                1 -> "#" // корабль
+                8 -> "X" // попадание
+                9 -> "o" // промах
+                else -> " "
+            }
+            print("$symbol ")
+        }
+        println()
+    }
+}
