234033860f1787c70891c2da1738ad76b7d884c8
max <nickitkacozloff@yandex.ru>
2025-01-31 03:30:13 +0300

    Add restricted area marking around ships

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,14 +3,16 @@ package org.example
 import kotlin.random.Random
 
 fun main() {
-    val playerField = Array(10) { Array(10) { 0 } }
-    val computerField = Array(10) { Array(10) { 0 } }
+    val playerField = Array(10) { IntArray(10) { 0 } }
+    val computerField = Array(10) { IntArray(10) { 0 } }
 
     println("Добро пожаловать в игру \"Морской бой\"!")
+
+    placeShips(playerField)
     printField(playerField)
 }
 
-fun printField(field: Array<Array<Int>>) {
+fun printField(field: Array<IntArray>) {
     val header = "  А Б В Г Д Е Ж З И К"
     println(header)
 
@@ -30,7 +32,7 @@ fun printField(field: Array<Array<Int>>) {
     }
 }
 
-fun placeShips(field: Array<Array<Int>>) {
+fun placeShips(field: Array<IntArray>) {
     val shipLengths = listOf(4, 3, 3, 2, 2, 2, 1, 1, 1, 1)
     for (shipLength in shipLengths) {
         var placed = false
@@ -47,7 +49,7 @@ fun placeShips(field: Array<Array<Int>>) {
     }
 }
 
-fun canPlaceShip(field: Array<Array<Int>>, row: Int, col: Int, size: Int, horizontal: Boolean): Boolean {
+fun canPlaceShip(field: Array<IntArray>, row: Int, col: Int, size: Int, horizontal: Boolean): Boolean {
     for (i in 0 until size) {
         val r = if (horizontal) row else row + i
         val c = if (horizontal) col + i else col
@@ -59,10 +61,32 @@ fun canPlaceShip(field: Array<Array<Int>>, row: Int, col: Int, size: Int, horizo
     return true
 }
 
-fun placeShip(field: Array<Array<Int>>, row: Int, col: Int, size: Int, horizontal: Boolean) {
+
+fun placeShip(field: Array<IntArray>, row: Int, col: Int, size: Int, horizontal: Boolean) {
     for (i in 0 until size) {
         val r = if (horizontal) row else row + i
         val c = if (horizontal) col + i else col
         field[r][c] = 1
+    }
+    markRestrictedArea(field, row, col, size, horizontal)
+}
+
+fun markRestrictedArea(field: Array<IntArray>, row: Int, col: Int, size: Int, horizontal: Boolean) {
+    val offset = listOf(-1, 0, 1)
+
+    for (i in 0 until size) {
+        val r = if (horizontal) row else row + i
+        val c = if (horizontal) col + i else col
+
+        for (dr in offset) {
+            for (dc in offset) {
+                val nr = r + dr
+                val nc = c + dc
+
+                if (nr in 0..9 && nc in 0..9 && field[nr][nc] == 0) {
+                    field[nr][nc] = 2
+                }
+            }
+        }
     }
 }
